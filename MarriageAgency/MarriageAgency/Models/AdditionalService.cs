f9c2d066d99942cb07563fd9cf814c271c34c74a
Konstantin <kevenmusic538@gmail.com>
2024-10-03 20:00:13 +0300

    add Models and DbContext

---
+++ MarriageAgency/MarriageAgency/Models/AdditionalService.cs
@@ -0,0 +1,14 @@
+﻿namespace MarriageAgency.Models;
+
+public partial class AdditionalService
+{
+    public int AdditionalServiceId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public virtual ICollection<Service> Services { get; set; } = new List<Service>();
+}
---
+++ MarriageAgency/MarriageAgency/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarriageAgency.Models
+{
+    public partial class Photo
+    {
+        [Display(Name = "Код клиента")]
+        public int ClientId { get; set; }
+
+        [Display(Name = "Фото клиента")]
+        public string ClientPhoto { get; set; } = null!;
+
+        [Display(Name = "Клиент")]
+        public virtual Client Client { get; set; } = null!;
+    }
+}
